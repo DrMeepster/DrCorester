92c49d4fbdeff40e72d0d75a51c6b5ba79e4b16b
DrMeepster <mgonikberg@yahoo.com>
2017-09-16 11:11:10 -0700

    Now only readme.md and docs

--- src/main/java/drmeepster/drcorester/common/block/IBasicBlock.java
+++
@@ -1,8 +0,0 @@
-package drmeepster.drcorester.common.block;
-
-import drmeepster.drcorester.common.util.IBasicObject;
-import net.minecraft.block.Block;
-
-public interface IBasicBlock extends IBasicObject<Block> {
-	public BasicItemBlock getItemBlock();
-}
